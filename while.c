a879ab67a4a060e24e995a8b2fe92d85e276fc64
udayvsp <146770886+udayvsp@users.noreply.github.com>
2023-11-02 20:04:49 +0530

    Add files via upload

---
+++ while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+    int main()
+
+{
+    int n,i=1;
+    scanf("%d",&n);//n=5-->1<=5:i+1==1+1=2
+    while (i<=n)
+    {
+        printf("i am learning c programming\n");
+        i++;
+    }
+    return 0;
+}
